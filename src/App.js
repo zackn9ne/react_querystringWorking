698f75feb3800cedf5d96319138ebc53c603d748
zackn9ne <zackn9ne@gmail.com>
2017-01-09 13:31:20 -0500

    adding another route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Router, Route, Link, hashHistory } from 'react-router';
 
+const About = () => <div><h1>about page header</h1></div>;
 const Page = (props) =>
 	<div><h1>{props.location.query.message || 'no mesage'}</h1></div>
 
@@ -9,6 +10,7 @@ class App extends React.Component {
 		return(
 			<Router history={ hashHistory }>
 				<Route path="/" component={Page}></Route>
+				<Route path="/about" component={About}></Route>
 			</Router>
 			)
 	}
